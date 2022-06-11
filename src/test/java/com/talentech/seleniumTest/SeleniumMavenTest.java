78812c26ff963929fa8f1a2e19fd0376ae1b159b
Juel <email.juel@gmail.com>
2022-06-11 15:52:45 -0400

    Fixed code. updated gitignore

--- src/test/java/com/talentech/seleniumTest/SeleniumMavenTest.java
+++ src/test/java/com/talentech/seleniumTest/SeleniumMavenTest.java
@@ -1,45 +1,39 @@
 package com.talentech.seleniumTest;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumMavenTest {
 
-	public static void main(String[] args) throws InterruptedException  {
-
-		WebDriver driver = new ChromeDriver();
+	public static void main(String[] args) throws InterruptedException {
 
 		if (System.getProperty("os.name").toLowerCase().contains("mac")) {
 			System.out.println("- Testing Started Mac Platform -");
-			System.setProperty("webdriver.chrome.driver", "chromedrivere");
+			System.setProperty("webdriver.chrome.driver", "chromedriver");
 		} else if (System.getProperty("os.name").toLowerCase().contains("win")) {
 			System.out.println("- Testing Started Windows Platform -");
-			System.setProperty("webdriver.chrome.driver", "chromedrivere.exe");
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		} else {
 			System.out.println("- Invalid Platform! -");
 		}
-
-		driver.get("https://www.etsy.com/");
+		// WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.ups.com/track?loc=en_US&requester=ST/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		WebElement signIn = driver.findElement(By.xpath("//button[contains(text(),'Sign in')]"));
-		signIn.click();
-		WebElement email = driver.findElement(By.cssSelector("#join_neu_email_field"));
-		WebElement password = driver.findElement(By.xpath("//input[@id='join_neu_password_field']"));
-		WebElement submitButton = driver.findElement(By.xpath("//button[@name='submit_attempt']"));
-		email.sendKeys("email@email.com");
-		password.sendKeys("password");
-		submitButton.click();
-		WebElement errorMsg = driver.findElement(By.xpath("//*[contains(text(),'Password was incorrect')]"));
+		WebElement trackingInput = driver.findElement(By.id("stApp_trackingNumber"));
+		trackingInput.sendKeys("123456789");
+		WebElement trackButton = driver.findElement(By.id("stApp_btnTrack"));
+		trackButton.click();
+		WebElement errorMsg = driver.findElement(By.id("stApp_lblInfoNotice"));
 		String error = errorMsg.getText().trim();
 
-		if (error.equals("Password was incorrect")) {
+		System.out.println(error);
+		if (error.equals("Please provide a tracking number.")) {
 			System.out.println("PASSED!");
 		} else {
 			System.out.println("FAILED!");
@@ -49,6 +43,5 @@ public class SeleniumMavenTest {
 		driver.quit();
 
 	}
-
 
 }
